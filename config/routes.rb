1d7faaf312e1c9b3eaa6ea966f712924710c7351
EC2 Default User <ec2-user@ip-172-31-5-10.ec2.internal>
2020-12-31 08:18:28 +0000

    [Add]admin customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
     root to: 'homes#top'
-    resources :items,  only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items,     only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres,    only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   devise_for :admins
   devise_for :customers
