083e29d17e5509d596094b6ea4dc0dc90921d2d0
EC2 Default User <ec2-user@ip-172-31-5-10.ec2.internal>
2021-02-16 07:19:03 +0000

    [Add]admin order show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,10 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_detail.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
     root to: 'homes#top'
-    resources :items,     only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres,    only: [:index, :create, :edit, :update]
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders,     only: [:index]
+    resources :items,      only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres,     only: [:index, :create, :edit, :update]
+    resources :customers,  only: [:index, :show, :edit, :update]
+    resources :orders,     only: [:index, :show, :update]
   end
   devise_for :admins
   devise_for :customers
