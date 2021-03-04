fefba4f5f115a4ddc3e9a561b14de922030a3419
EC2 Default User <ec2-user@ip-172-31-5-10.ec2.internal>
2021-03-04 18:53:12 +0000

    [Add]customer inactive

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,4 +2,14 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(current_customer.id)
   end
+
+  def inactive
+  end
+
+  def disable
+    customer = Customer.find(current_customer.id)
+    customer.is_deleted = TRUE
+    customer.update
+    redirect_to destroy_customer_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,23 @@ Rails.application.routes.draw do
   root to: 'public/homes#top'
   scope module: :public do
     get 'about' => 'homes#about'
-    resources :items,         only: [:index, :show]
-    resources :cart_items,    only: [:index, :create]
-    resources :orders,        only: [:new, :create]
-    post 'orders/confirm'   => 'orders#confirm'
-    get  'orders/complete'  => 'orders#complete'
-    resources :addresses,     only: [:index, :create]
-    get  'customers/mypage' => 'customers#show'
+    resources :items,             only: [:index, :show]
+    resources :cart_items,        only: [:index, :create]
+    resources :orders,            only: [:new, :create]
+    post 'orders/confirm'     => 'orders#confirm'
+    get  'orders/complete'    => 'orders#complete'
+    resources :addresses,         only: [:index, :create]
+    get  'customers/mypage'   => 'customers#show'
+    get  'customers/inactive' => 'customers#inactive'
+    patch 'customers/disable' => 'customers#disable'
   end
   namespace :admin do
     root to: 'homes#top'
-    resources :items,         only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres,        only: [:index, :create, :edit, :update]
-    resources :customers,     only: [:index, :show, :edit, :update]
-    resources :orders,        only: [:index, :show, :update]
-    resources :order_details, only: [:update]
+    resources :items,             only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres,            only: [:index, :create, :edit, :update]
+    resources :customers,         only: [:index, :show, :edit, :update]
+    resources :orders,            only: [:index, :show, :update]
+    resources :order_details,     only: [:update]
   end
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
